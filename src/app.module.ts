0af3366033ebe8b7bad580e322853b663c683597
[AI7] 민준영 <znddiqjwjs@gmail.com>
2023-07-22 18:20:51 +0900

    Feat : JWT 사용 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import ormconfig from './orm.config';
+
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './auth/jwt/jwt.strategy';
+import { UserService } from './user/user.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot(ormconfig),
+    // session을 사용하지 않을 예정이기 때문에 false
+    PassportModule.register({ defaultStrategy: 'jwt', session: false }),
+    // jwt 생성할 때 사용할 시크릿 키와 만료일자 적어주기
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '1y' },
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, JwtStrategy, UserService],
 })
 export class AppModule {}

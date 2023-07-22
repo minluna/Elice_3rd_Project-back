f4dcab2accf9b0e58f27c3b3cfb1c8848dc5167a
[AI7] 민준영 <znddiqjwjs@gmail.com>
2023-07-22 18:20:26 +0900

    Feat : 입력값 검증 설정

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Swagger 설정
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
@@ -13,6 +15,15 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  // 입력값 검증 설정
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true, //형식 안맞으면 거절
+      transform: true, //자동 자료형 변환
+    }),
+  );
 
   await app.listen(3000);
 }

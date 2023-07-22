71302c0c81d819973ea4ed875f3bdc815f78af19
[AI7] 민준영 <znddiqjwjs@gmail.com>
2023-07-22 18:17:26 +0900

    Feat : 유저 생성, 수정 시 형식 설정

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: '이메일을 입력하세요.' })
+  @IsEmail({}, { message: '유효한 이메일을 입력하세요.' })
+  email: string;
+
+  @IsNotEmpty({ message: '비밀번호를 입력하세요.' })
+  @MinLength(10, { message: '비밀번호는 최소 10자리 이상이어야 합니다.' })
+  password: string;
+
+  @IsNotEmpty({ message: '이름을 입력하세요.' })
+  nickname: string;
+
+  @IsNotEmpty({ message: 'imageUrl을 확인하세요.' })
+  imageUrl: string;
+}
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsOptional()
+  @IsNotEmpty({ message: '설명을 입력하세요.' })
+  description: string;
+
+  @IsOptional()
+  @IsNotEmpty({ message: '이미지 파일은 필수 입력 사항입니다.' })
+  imageUrl?: any;
+}

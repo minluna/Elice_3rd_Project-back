5d0cd8bad33d0f23e1c1114980fbbe15a1fa0e35
[AI7] 민준영 <znddiqjwjs@gmail.com>
2023-07-25 00:55:55 +0900

    Feat : entities와 multer 모듈 import

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { User } from '../user/entities/user.entity';
+import { Post } from './entities/post.entity';
+import { PostImage } from './entities/post-image.entity';
+
+import { MulterModule } from '@nestjs/platform-express';
+import { multerOptionsFactory } from 'src/utils/multer.options';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Post, PostImage, User]),
+    MulterModule.registerAsync({
+      imports: [],
+      useFactory: multerOptionsFactory,
+      inject: [],
+    }),
+  ],
+  exports: [TypeOrmModule],
   controllers: [PostController],
-  providers: [PostService]
+  providers: [PostService],
 })
 export class PostModule {}

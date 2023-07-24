51241026e30cff3a662cbdb2da925000ed0db568
[AI7] 민준영 <znddiqjwjs@gmail.com>
2023-07-25 00:58:20 +0900

    Feat : point 테이블 컬럼별 설명 추가

--- src/user/entities/point.entity.ts
+++ src/user/entities/point.entity.ts
@@ -3,16 +3,16 @@ import { User } from './user.entity';
 
 @Entity({ name: 'point' })
 export class Point {
-  @PrimaryGeneratedColumn({ type: 'int' })
+  @PrimaryGeneratedColumn({ type: 'int', comment: 'point의 ID' })
   pointId: number;
 
-  @Column({ type: 'int' })
+  @Column({ type: 'int', comment: 'point의 유저 ID' })
   userId: number;
 
-  @Column({ type: 'int' })
+  @Column({ type: 'int', comment: '유저의 현재 포인트' })
   currentPoint: number;
 
-  @Column({ type: 'int' })
+  @Column({ type: 'int', comment: '유저의 누적 포인트' })
   accuPoint: number;
 
   @OneToOne(() => User, (user) => user.point)

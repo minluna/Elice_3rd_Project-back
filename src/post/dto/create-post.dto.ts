3088c46d6dfc1749670854edf2fb6c5e519d34cf
[AI7] 민준영 <znddiqjwjs@gmail.com>
2023-07-25 00:56:29 +0900

    Feat : post 생성 시 필요한 데이터 유효성검사

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,1 +1,11 @@
-export class CreatePostDto {}
+import { IsNotEmpty, IsOptional, MaxLength } from 'class-validator';
+
+export class CreatePostDto {
+  @IsNotEmpty({ message: '게시물 내용을 입력하세요.' })
+  @MaxLength(200, { message: '게시물 내용은 최대 200글자까지 허용됩니다.' })
+  content: string;
+
+  @IsOptional()
+  @IsNotEmpty({ message: '이미지 파일은 필수 입력 사항입니다.' })
+  imageUrl?: any;
+}
